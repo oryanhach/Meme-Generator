8d4cdbf7404238a452fe4d233ce74c43996b828c
Oryan Hachamoff <oryanhach95@gmail.com>
2023-06-26 08:44:53 +0300

    removed about page

--- js/pages.controller.js
+++ js/pages.controller.js
@@ -1,46 +1,25 @@
 'use strict'
 
-function goToGenerator(idx) {
+function goToGenerator(idx){
     const elSearchBar = document.querySelector('.search-bar')
     const elGallery = document.querySelector('.gallery-container')
     const elGenerator = document.querySelector('.generator-layout')
-    const elAbout = document.querySelector('.about-page')
 
     elSearchBar.style.display = 'none'
     elGallery.style.display = 'none'
-    elAbout.style.display = 'none'
-
+    
     elGenerator.style.display = 'grid'
     initCanvas()
     renderImage(idx)
 }
 
-function goToGallery() {
+function goToGallery(){
     const elSearchBar = document.querySelector('.search-bar')
     const elGallery = document.querySelector('.gallery-container')
     const elGenerator = document.querySelector('.generator-layout')
-    const elAbout = document.querySelector('.about-page')
-
 
     elGenerator.style.display = 'none'
-    elAbout.style.display = 'none'
-
-
+    
     elSearchBar.style.display = 'block'
     elGallery.style.display = 'flex'
 }
-
-function goToAbout() {
-    const elSearchBar = document.querySelector('.search-bar')
-    const elGallery = document.querySelector('.gallery-container')
-    const elGenerator = document.querySelector('.generator-layout')
-    const elAbout = document.querySelector('.about-page')
-
-
-    elSearchBar.style.display = 'none'
-    elGallery.style.display = 'none'
-    elGenerator.style.display = 'none'
-
-    elAbout.style.display = 'block'
-
- }
